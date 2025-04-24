77244e2ede6bfa76f9ed6633da5666275b615210
krishna subramaniyan <krishnasubramaniyan2001@gmail.com>
2025-04-24 17:43:34 +0530

    create label

--- basic/label.py
+++ basic/label.py
@@ -3,7 +3,7 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
 
 # find center coordinates
-def center(width,height,winWidth,winHeight) -> tuple:
+def centerCoordinate(width,height,winWidth,winHeight) -> tuple:
     width = (width-winWidth)//2
     height = (height-winHeight)//2
     return (width, height)
@@ -11,16 +11,16 @@ def center(width,height,winWidth,winHeight) -> tuple:
 def window() -> None:
     app = QApplication(sys.argv)
     win = QMainWindow()
-    cord = center(1920,1080,300,300)
-    win.setGeometry(cord[0],cord[1],300,300)
+    centerPoint = centerCoordinate(1920,1080,300,300)
+    win.setGeometry(centerPoint[0],centerPoint[1],300,300)
     win.setWindowTitle("Qt application")
 
     label = QtWidgets.QLabel(win)
     label.setText("my first label")
     shape = label.size()
-    centerCord = center(300,300,shape.width(),shape.height())
+    coordinate = centerCoordinate(300,300,shape.width(),shape.height())
 
-    label.move(centerCord[0],centerCord[1])
+    label.move(coordinate[0],coordinate[1])
 
     win.show()
     sys.exit(app.exec())
