7232676eff46c3ff6249e74480c367ff3e115227
krishna subramaniyan <krishnasubramaniyan2001@gmail.com>
2025-04-24 13:13:07 +0530

    qt label

---
+++ basic/label.py
@@ -0,0 +1,28 @@
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QApplication, QMainWindow
+import sys
+
+# find center coordinates
+def center(width,height,winWidth,winHeight) -> tuple:
+    width = (width-winWidth)//2
+    height = (height-winHeight)//2
+    return (width, height)
+
+def window() -> None:
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    cord = center(1920,1080,300,300)
+    win.setGeometry(cord[0],cord[1],300,300)
+    win.setWindowTitle("Qt application")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("my first label")
+    shape = label.size()
+    centerCord = center(300,300,shape.width(),shape.height())
+
+    label.move(centerCord[0],centerCord[1])
+
+    win.show()
+    sys.exit(app.exec())
+
+window()
